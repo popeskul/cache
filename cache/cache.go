ca06dfeca44a39711b03974b72f8ede4005b6f87
Pavel Popeskul <peterjj91@gmail.com>
2022-07-18 22:46:02 +0300

    basic-implementation: ✨ add cache feature

---
+++ cache/cache.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"sync"
+)
+
+type db struct {
+	data map[string]interface{}
+	sync.Mutex
+}
+
+func New(data map[string]interface{}) *db {
+	return &db{
+		data: data,
+	}
+}
+
+func (db *db) Set(key string, value interface{}) {
+	db.Lock()
+	defer db.Unlock()
+
+	db.data[key] = value
+}
+
+func (db *db) Get(key string) (result interface{}) {
+	db.Lock()
+	defer db.Unlock()
+
+	return db.data[key]
+}
+
+func (db *db) Delete(key string) {
+	db.Lock()
+	defer db.Unlock()
+
+	delete(db.data, key)
+}
